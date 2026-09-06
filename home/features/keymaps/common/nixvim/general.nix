18d4e4448184354e75285c37e6803602c2effbc7
Abreu <316380489+o-abreu@users.noreply.github.com>
2026-09-06 20:44:47 -0300

    feat(nixvim): Added keybind to delete al preceding whitespace

--- home/features/keymaps/common/nixvim/general.nix
+++ home/features/keymaps/common/nixvim/general.nix
@@ -65,5 +65,26 @@
       options.desc = "Sort";
       mode = "v";
     }
+
+    {
+      mode = "i";
+      key = "<C-H>";
+      action.__raw = ''
+        function()
+          local col = vim.fn.col('.') - 1
+          if col == 0 then return end
+
+          local line = vim.fn.getline('.')
+          local left_text = line:sub(1, col)
+          local spaces = left_text:match("(%s+)$")
+
+          if spaces then
+            local row = vim.fn.line('.') - 1
+            vim.api.nvim_buf_set_text(0, row, col - #spaces, row, col, {""})
+          end
+        end
+      '';
+      options.desc = "Delete preceding whitespace";
+    }
   ];
 }
