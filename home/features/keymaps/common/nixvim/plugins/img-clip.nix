290acc10c5a1716131e18d128a1a46738c989ae8
Abreu <316380489+o-abreu@users.noreply.github.com>
2026-09-17 21:07:53 -0300

    feat(nixvim): paste images in latex, typst and quarto

--- home/features/keymaps/common/nixvim/plugins/img-clip.nix
+++ home/features/keymaps/common/nixvim/plugins/img-clip.nix
@@ -6,19 +6,29 @@
 }:
 {
   config = lib.optionalAttrs (options ? programs.nixvim) {
-    programs.nixvim.files."ftplugin/markdown.lua".keymaps =
-      lib.mkIf config.programs.nixvim.plugins.img-clip.enable
-        [
-          {
-            key = "<localleader>P";
-            action = "<cmd>PasteImage<cr>";
-            mode = "n";
-            options = {
-              buffer = true;
-              silent = true;
-              desc = "Paste image from system clipboard";
-            };
-          }
+    programs.nixvim.files =
+      let
+        cfg = config.programs.nixvim.plugins.img-clip;
+        filetypes = [
+          "markdown"
+          "latex"
+          "typst"
+          "quarto"
         ];
+        keybind = {
+          key = "<localleader>P";
+          action = "<cmd>PasteImage<cr>";
+          mode = "n";
+          options = {
+            buffer = true;
+            silent = true;
+            desc = "Paste image from system clipboard";
+          };
+        };
+      in
+      map (elem: "ftplugin/${elem}.lua") filetypes
+      |> lib.flip lib.genAttrs (_: {
+        keymaps = lib.optional cfg.enable keybind;
+      });
   };
 }
